63bdf96642edbea7b7d8f53dc20f6d79fb8aec83
kchauhan <kanika.chauhan@ymail.com>
2022-08-15 19:05:45 +0530

    initial commit

---
+++ test/com/daa/practice/MainTest.java
@@ -0,0 +1,14 @@
+package com.daa.practice;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
